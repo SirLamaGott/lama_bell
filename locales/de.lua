83ee48a8406806af367f809db689f81ffb9e5d23
John <65968077+SirLamaGott@users.noreply.github.com>
2022-04-04 21:36:45 +0200

    Add files via upload

---
+++ locales/de.lua
@@ -0,0 +1,10 @@
+Locales ['de'] = {
+  ['trigger'] = 'Drücke ~INPUT_CONTEXT~ um zu Klingeln.',
+  ['used'] = 'Du hast die ~b~Klingel~s~ betätigt.',
+  ['wait'] = 'Du musst warten, bevor du die ~b~Klingel~s~ erneut nutzen kannst.',
+  
+  ['ring'] = 'Klingel',
+  ['title'] = 'Titel',
+  ['ringed'] = 'Jemand hat geklingelt',
+}
+
---
+++ locales/en.lua
@@ -0,0 +1,10 @@
+Locales ['en'] = {
+  ['trigger'] = 'Press ~INPUT_CONTEXT~ to use the bell.',
+  ['used'] = 'You have used the ~b~bell.',
+  ['wait'] = 'You have to wait before you can use the ~b~bell~s~ again!',
+  
+  ['bell'] = 'Bell',
+  ['title'] = 'Titel',
+  ['belled'] = 'Jemand hat geklingelt',
+}
+
